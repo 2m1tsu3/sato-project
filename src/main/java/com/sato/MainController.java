87fb63cd4068974d8ddfdcc4200620d08fa5b4ac
unknown <>
2017-06-24 14:15:24 +0900

    mybatis..

--- src/main/java/com/sato/MainController.java
+++ src/main/java/com/sato/MainController.java
@@ -2,6 +2,7 @@ package com.sato;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,10 @@ import com.sato.dto.tbl.MasterTblDto;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	Dao dao;
+	
     @RequestMapping("/")
     @ResponseBody
     List<MasterTblDto> home() {
--- src/main/java/com/sato/dao/Dao.java
+++ src/main/java/com/sato/dao/Dao.java
@@ -2,8 +2,11 @@ package com.sato.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.sato.dto.tbl.MasterTblDto;
 
+@Component
 public interface Dao {
 	public List<MasterTblDto> selectAll();
 
